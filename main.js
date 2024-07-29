9f8c51f106bccf246871dccac4da7dc4d587760c
Pablolu2024 <160808920+Pablolu2024@users.noreply.github.com>
2024-07-29 16:27:47 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
 const MODEL_URL = './models';
 
-const imgUrl = '/images/${label}.jpg';
+
 
 document.addEventListener("DOMContentLoaded", async () => {
     const video = document.getElementById('video');
@@ -54,10 +54,10 @@ document.addEventListener("DOMContentLoaded", async () => {
     });
 
     async function loadLabeledImages() {
-        
+        const labels = ['person1','person2']; // Cambia estos nombres a los de tus imágenes
         return Promise.all(
             labels.map(async label => {
-               const labels = ['person1','person2']; // Cambia estos nombres a los de tus imágenes 
+                const imgUrl = './images/${label}.jpg';
                 const img = await faceapi.fetchImage(imgUrl);
                 const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor();
                 if (!detections) {
